c229c449a7194ac562a5da306f9116891f439b4c
OmarFarouq3 <oabdulsalam729@gmail.com>
2026-03-27 18:24:01 +0400

    Delete Omar/distance.py

--- Omar/distance.py
+++
@@ -1,6 +0,0 @@
-def total_distance(routes, distance_matrix):
-    total = 0
-    for route in routes:
-        for i in range(len(route) - 1):
-            total += distance_matrix[route[i]][route[i+1]]
-    return total
